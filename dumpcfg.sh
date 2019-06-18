6a3b0e9a8308da7f6d058612f9d20932c05537b5
Pachurii <ALPachurii@protonmail.com>
2019-06-18 13:22:43 +0800

    style(dumpcfg): Print the processing file to stdout

--- dumpcfg.sh
+++ dumpcfg.sh
@@ -6,11 +6,13 @@ for region in "${regionList[@]}"; do
     find AzurLane_ClientSource/Src/"$region"/sharecfg -type f | while read file ; do
         path="$(echo $file | sed 's/.lua//g')"
         name=$(basename $file '.lua')
-        env lua5.3 serializer.lua "$path" "$name" | jq . | tee "Data_Dump/$path"
+	echo "Processing file: $file"
+        env lua5.3 serializer.lua "$path" "$name" | jq . | tee "Data_Dump/$path" > /dev/null
     done
 
     find AzurLane_ClientSource/Src/"$region"/gamecfg -type f \( ! -iname "academygraph.lua" ! -iname "assistantinfo.lua" ! -iname "chargeshiptalkinfo.lua" ! -iname "include.lua" ! -iname "names.lua" \) | while read file ; do
         path=$(echo "$file" | sed 's/.lua//g')
-        env lua5.3 serializer2.lua "$path" | jq . | tee "Data_Dump/$path"
+	echo "Processing file: $file"
+        env lua5.3 serializer2.lua "$path" | jq . | tee "Data_Dump/$path" > /dev/null
     done
 done
